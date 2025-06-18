6e243474ba0f0e3d4e5a88675e148e5b39708185
axiomproject <johncharlesvergara01@gmail.com>
2025-06-18 12:47:35 +0800

    test

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -1,6 +1,13 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="navbar">
       <div className="navbar-inner">
@@ -9,11 +16,14 @@ const Navbar = () => {
             <img src="/Logo.png" alt="Aeriscent Logo" className="logo-image" />
           </Link>
         </div>
-        <nav className="nav-links">
-          <Link to="/">HOME</Link>
-          <Link to="/collection">COLLECTION</Link>
-          <Link to="/new-arrivals">NEW ARRIVALS</Link>
-          <Link to="/pre-order">PRE-ORDER</Link>
+        <button className="mobile-menu-btn" onClick={toggleMenu}>
+          <span className={`hamburger ${isMenuOpen ? 'open' : ''}`}></span>
+        </button>
+        <nav className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
+          <Link to="/" onClick={() => setIsMenuOpen(false)}>HOME</Link>
+          <Link to="/collection" onClick={() => setIsMenuOpen(false)}>COLLECTION</Link>
+          <Link to="/new-arrivals" onClick={() => setIsMenuOpen(false)}>NEW ARRIVALS</Link>
+          <Link to="/pre-order" onClick={() => setIsMenuOpen(false)}>PRE-ORDER</Link>
         </nav>
       </div>
     </header>
